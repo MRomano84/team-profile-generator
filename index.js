b211d185db85036be956a91039405d0cde719424
Matthew Romano <mathiasrok@gmail.com>
2021-02-25 19:57:02 -0500

    outline

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { functionExpression } = require("@babel/types");
 
-console.log("Check if working");
+
+//Prompt to create a new employee
+function newEmployee() {
+
+}
+
+
+
+//Prompt for creating a Manager
+function promptManager() {
+
+}
+
+
+
+//Prompt for creating a Engineer
+function promptEngineer() {
+
+}
+
+
+
+//Prompt for creating an Intern
+function promptIntern() {
+
+}
+
+
+//Need to create the HTML to display Employee cards
