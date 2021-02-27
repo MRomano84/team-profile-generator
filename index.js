defe8ab19e160cda9b29cf93ae5be0283323d91d
Matthew Romano <mathiasrok@gmail.com>
2021-02-27 18:15:41 -0500

    inq prompts

--- index.js
+++ index.js
@@ -4,20 +4,88 @@ const fs = require("fs");
 
 //Prompt to create a new employee
 function newEmployee() {
-
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Employee card creator. Please select a role:',
+            choices: [
+                'Manager',
+                'Engineer',
+                'Intern',
+                'Done'
+            ]
+        },
+    ])
+    .then((answers) => {
+        switch (answers.role) {
+            case 'Manager':
+                promptManager()
+                break;
+            case 'Engineer':
+                promptEngineer()
+                break;
+            case 'Intern':
+                promptIntern()
+                break;
+            case 'Done':
+                //...function that will make the html page - I think
+        }
+    })
 }
 
 
 
 //Prompt for creating a Manager
 function promptManager() {
-
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Manager\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Manager\'s id#:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Manager\'s email address:'
+        },
+        {
+            type: 'number',
+            name: 'officeNumber',
+            message: 'Enter the Manager\'s office number:'
+        }
+    ])
 }
-
-
 
 //Prompt for creating a Engineer
 function promptEngineer() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Engineer\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Engineer\'s id#:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Engineer\'s email address:'
+        },
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: 'Enter the Engineer\'s GitHub Username:'
+        }
+    ])
 
 }
 
@@ -25,8 +93,29 @@ function promptEngineer() {
 
 //Prompt for creating an Intern
 function promptIntern() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Intern\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Intern\'s id#:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Intern\'s email address:'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Where is the Intern attending college?'
+        }
+    ])
 
 }
-
 
 //Need to create the HTML to display Employee cards
